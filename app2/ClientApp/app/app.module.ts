66fac669c0d1659e6feef5b67b5d72cdfc184498
John Williams <jwilliams@core-tt.net>
2017-03-23 16:13:44 +1300

    Initial Commit

---
+++ app2/ClientApp/app/app.module.ts
@@ -0,0 +1,38 @@
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { UniversalModule } from 'angular2-universal';
+import { AppComponent } from './components/app/app.component'
+import { NavMenuComponent } from './components/navmenu/navmenu.component';
+import { HomeComponent } from './components/home/home.component';
+import { FetchDataComponent } from './components/fetchdata/fetchdata.component';
+import { CounterComponent } from './components/counter/counter.component';
+import { HeroComponent } from './components/heroes/heroes.component';
+import { FormsModule } from '@angular/forms';
+
+@NgModule({
+    bootstrap: [ AppComponent ],
+    declarations: [
+        AppComponent,
+        NavMenuComponent,
+        CounterComponent,
+        FetchDataComponent,
+        HomeComponent, 
+        HeroComponent
+    ],
+    imports: [
+        UniversalModule, // Must be first import. This automatically imports BrowserModule, HttpModule, and JsonpModule too.
+        FormsModule,
+        RouterModule.forRoot([
+            { path: '', redirectTo: 'home', pathMatch: 'full' },
+            { path: 'home', component: HomeComponent },
+            { path: 'counter', component: CounterComponent },
+            { path: 'fetch-data', component: FetchDataComponent },
+            { path: 'hero', component: HeroComponent },
+            { path: '**', redirectTo: 'home' },
+            
+            //component: CounterComponent
+        ])
+    ]
+})
+export class AppModule {
+}
